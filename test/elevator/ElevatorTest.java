f7d356e9bb62281f010dec1c4179e3c722d4c7e2
sillyfunnypedro <sillyfunnypedro@gmail.com>
2024-03-08 11:32:14 -0800

    Cleaning up checkstyle

--- test/elevator/ElevatorTest.java
+++ test/elevator/ElevatorTest.java
@@ -1,7 +1,6 @@
 package elevator;
 
 import building.enums.Direction;
-import building.enums.ElevatorStatus;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
